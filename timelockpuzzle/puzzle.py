c184a1e9e59ef4bd1013fce80151cf62a1625553
Jonathan Levi <jonathan1996levi@gmail.com>
2019-02-06 18:12:57 +0000

    rearrange code, add explanatory comments

--- timelockpuzzle/puzzle.py
+++ timelockpuzzle/puzzle.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import time
 
 from timelockpuzzle.algorithms.fast_exponentiation import fast_exponentiation
 
@@ -10,44 +9,35 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 
 
 # TODO: move to class
-def encrypt(seconds: int, squarings_per_second: int):
-    # TODO: find safe seconds to use
+def encrypt(message: bytes, seconds: int, squarings_per_second: int):
     if not seconds or not squarings_per_second:
-        # TODO: custom error handling
         raise AssertionError
 
-    # message contains vote info
-    message = b"This is a vote for Myrto"
-
+    # hard code safe exponent to use
     private_key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048,
         backend=default_backend()
     )
 
-    # used to compute phi_n
+    # see RSA for security specifications
     p, q = private_key.private_numbers().p, private_key.private_numbers().q
-
-    # save for later
     n = private_key.public_key().public_numbers().n
+    phi_n = (p - 1) * (q - 1)
 
-    # encrypt vote
     # Fernet is an asymmetric encryption protocol using AES
-    # TODO: this key is probably unsafe.
-    # TODO: Fernet uses AES under the hood. Maybe use directly?
     key = Fernet.generate_key()
     key_int = int.from_bytes(key, sys.byteorder)
-
     cipher_suite = Fernet(key)
+
+    # Vote Encryption
     encrypted_message = cipher_suite.encrypt(message)
 
-    # pick random a
-    # TODO: to helper. Choose fixed 2, as mentioned in the paper?
+    # Pick safe, pseudo-random a where 1 < a < n
+    # Alternatively, we could use a = 2
     a = int.from_bytes(os.urandom(32), sys.byteorder) % n + 1
 
-    # Encrypt key
-    # Hardness assumption: factoring phi_n
-    phi_n = (p - 1) * (q - 1)
+    # Key Encryption
     t = seconds * squarings_per_second
     e = 2**t % phi_n
     b = fast_exponentiation(n, a, e)
@@ -57,13 +47,14 @@ def encrypt(seconds: int, squarings_per_second: int):
 
 
 def decrypt(n: int, a: int, t: int, enc_key: int, enc_message: int):
-    before = time.time()
+    # Successive squaring to find b
+    # We assume this cannot be parallelized
     b = a % n
     for i in range(t):
         b = b**2 % n
-    print('It took:', time.time() - before)
     dec_key = (enc_key - b) % n
 
+    # Retrieve key, decrypt message
     key_bytes = int.to_bytes(dec_key, length=64, byteorder=sys.byteorder)
     cipher_suite = Fernet(key_bytes)
     return cipher_suite.decrypt(enc_message)
@@ -75,9 +66,7 @@ if __name__ == '__main__':
     arg_t, arg_s = sys.argv[1], sys.argv[2]
     print("t =", arg_t)
     print("s =", arg_s)
-    # TODO: function for time counting
-    n, a, t, encrypted_key, encrypted_message, original_key = encrypt(int(arg_t), int(arg_s))
+    n, a, t, encrypted_key, encrypted_message, original_key = encrypt(b"This is a vote for Myrto", int(arg_t), int(arg_s))
     print('Decrypting')
-    # decrypt
     dec_msg = decrypt(n, a, t, encrypted_key, encrypted_message)
     print(dec_msg)
